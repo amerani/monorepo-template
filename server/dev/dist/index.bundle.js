5e398d62be071e74baebe6949aacc3195df0e95c
Alek Merani <alekmerani@gmail.com>
2022-02-27 17:54:39 -0800

    add cra and webpack

---
+++ server/dev/dist/index.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('hello world');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEscUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9AbXljb21wYW55L3NlcnZlci1kZXYvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiYWxlcnQoJ2hlbGxvIHdvcmxkJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
